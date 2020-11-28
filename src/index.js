85df08f9071fcbba0c553e9e53c612ee5e6e8a8f
vostbur <vostbur@gmail.com>
2020-11-28 12:37:51 +0300

    make main components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-    <div>
-        <h1>Daily Tasks</h1>
-        <input placeholder='search' />
-        <ul>
-            <li>Start training react app</li>
-            <li>Make coffee</li>
-        </ul>
-    </div>
-);
+const Header = () => {
+    return <h1>Daily Tasks</h1>;
+};
 
-ReactDOM.render(el,
+const SearchBar = () => {
+    const searchText = 'Type here for search';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+
+    return <input
+        placeholder={searchText}
+        style={searchStyle} />;
+};
+
+const Tasks = () => {
+    return <ul>
+        <li>Start training react app</li>
+        <li>Make coffee</li>
+    </ul>;
+};
+
+const App = () => {
+    return <div>
+        <Header />
+        <SearchBar />
+        <Tasks />
+    </div>;
+};
+
+ReactDOM.render(<App />,
     document.getElementById('root'));
 
