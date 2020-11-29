f5ac552d33afc9e6fe66b70d27754bf29db914f4
vostbur <vostbur@gmail.com>
2020-11-29 09:55:57 +0300

    refactor TaskItem

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -1,40 +1,38 @@
 import React, { Component } from 'react';
 import './TaskItem.css';
 
-export default class TaskItem extends Component {
-    render() {
-        const { name, onDeleteClick, onDoneClick, 
-                onImportantClick, done, important } = this.props;
+const TaskItem = ({ name, onDeleteClick, onDoneClick,
+    onImportantClick, done, important }) => {
 
-        let classNames = 'task-item';
-        if (done) {
-            classNames += ' done';
-        };
-        if (important) {
-            classNames += ' important';
-        };
+    let classNames = 'task-item';
+    if (done) {
+        classNames += ' done';
+    };
+    if (important) {
+        classNames += ' important';
+    };
 
-        return (
-            <span className={classNames}>
-                <span
-                    className='task-item-name'
-                    onClick={onDoneClick}>
-                    {name}
-                </span>
-
-                <button type="button"
-                    className="btn btn-outline-success btn-sm float-right"
-                    onClick={onImportantClick}>
-                    <i className="fa fa-exclamation" />
-                </button>
-
-                <button type="button"
-                    className="btn btn-outline-danger btn-sm float-right"
-                    onClick={onDeleteClick}>
-                    <i className="fa fa-trash-o" />
-                </button>
-
+    return (
+        <span className={classNames}>
+            <span
+                className='task-item-name'
+                onClick={onDoneClick}>
+                {name}
             </span>
-        );
-    }
-}
+
+            <button type="button"
+                className="btn btn-outline-success btn-sm float-right"
+                onClick={onImportantClick}>
+                <i className="fa fa-exclamation" />
+            </button>
+
+            <button type="button"
+                className="btn btn-outline-danger btn-sm float-right"
+                onClick={onDeleteClick}>
+                <i className="fa fa-trash-o" />
+            </button>
+        </span>
+    );
+};
+
+export default TaskItem;
